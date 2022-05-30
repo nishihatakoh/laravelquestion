7433bdba180ecf666f7f69d5b33575192476d3e6
nishihatakoh <koh.s13.kk@gmail.com>
2022-05-30 18:55:33 +0900

    second-commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,15 @@ class TestController extends Controller
     public function index()
     {
         $item = [
-            'content' => '本文',
+            'content' => '自由に入力してください',
+        ];
+        return view('index', $item);
+    }
+    public function post(Request $request)
+    {
+        $content = $request->content;
+        $item = [
+            'content' => $content . 'と入力しましたね'
         ];
         return view('index', $item);
     }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
 Route::get('/', [TestController::class, 'index']);
+Route::post('/', [TestController::class, 'post'])->middleware(FirstMiddleware::class);
